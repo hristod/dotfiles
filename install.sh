ee452fbbc80c90fca201d299bedf5eeb83b6568b
Hristo Dimitrov <hristo.dimitrov.dimitrov@gmail.com>
2017-08-10 00:00:58 +0300

    added install.sh from https://github.com/Remedan/dotfiles

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
-ln -s ~/dotfiles/.* ~
+#!/bin/bash
+
+mkdir -p ~/.config
+
+mkdir -p ~/dotfiles_old/.config
+
+cd ~/dotfiles
+
+BLACKLIST="-I install.sh -I .config -I .git -I .gitignore -I .gitmodules -I README.md -I screenshot.png"
+
+for file in `ls -A $BLACKLIST`
+do
+    mv ~/$file ~/dotfiles_old
+    ln -s ~/dotfiles/$file ~/$file
+done
+
+for file in `ls -A -I .config .config`
+do
+    mv ~/.config/$file ~/dotfiles_old/.config
+    ln -s ~/dotfiles/.config/$file ~/.config/$file
+done
+
+git submodule update --init --recursive
